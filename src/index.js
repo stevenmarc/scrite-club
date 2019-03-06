3854f36e33e1d6d6b42e10ffba59a8d4988c633b
stevenmarc <stevenmarcfr@gmail.com>
2019-03-06 13:37:23 +0100

    Add Router

--- src/index.js
+++ src/index.js
@@ -4,5 +4,15 @@ import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+const Root = () => {
+  <Router>
+    <Switch>
+      <Route path='/' component={App} />>
+    </Switch>
+  </Router>;
+};
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
