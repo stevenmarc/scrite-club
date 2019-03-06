34a83fba296e9c10ffe40acb6eaa1d3246f54637
stevenmarc <stevenmarcfr@gmail.com>
2019-03-06 13:48:16 +0100

    Add Auth components and routes

--- src/index.js
+++ src/index.js
@@ -2,17 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-const Root = () => {
+const Root = () => (
   <Router>
     <Switch>
-      <Route path='/' component={App} />>
+      <Route exact path='/' component={App} />>
+      <Route path='/login' component={Login} />
+      <Route path='/register' component={Register} />
     </Switch>
-  </Router>;
-};
+  </Router>
+);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
