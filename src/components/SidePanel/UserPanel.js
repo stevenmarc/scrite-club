022ba7e1ba316497b83352aa85561833fc790431
stevenmarc <stevenmarcfr@gmail.com>
2019-03-11 16:25:45 +0100

    Passing redux state via props

--- src/components/SidePanel/UserPanel.js
+++ src/components/SidePanel/UserPanel.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 import firebase from '../../firebase';
+import { Grid, Header, Icon, Dropdown } from 'semantic-ui-react';
 
 class UserPanel extends React.Component {
   dropdownOptions = () => [
@@ -8,12 +8,15 @@ class UserPanel extends React.Component {
       key: 'user',
       text: (
         <span>
-          Signed in as <strong>User</strong>
+          Signed in as <strong>{this.props.currentUser.displayName}</strong>
         </span>
       ),
       disabled: true
     },
-    { key: 'avatar', text: <span>Change Avatar</span> },
+    {
+      key: 'avatar',
+      text: <span>Change Avatar</span>
+    },
     {
       key: 'signout',
       text: <span onClick={this.handleSignout}>Sign Out</span>
@@ -24,24 +27,25 @@ class UserPanel extends React.Component {
     firebase
       .auth()
       .signOut()
-      .then(() => console.log('signed out'));
+      .then(() => console.log('signed out!'));
   };
 
   render() {
     return (
-      <Grid style={{ background: '#4c34c' }}>
+      <Grid style={{ background: '#4c3c4c' }}>
         <Grid.Column>
           <Grid.Row style={{ padding: '1.2em', margin: 0 }}>
-            {/*App Header */}
+            {/* App Header */}
             <Header inverted floated='left' as='h2'>
               <Icon name='code' />
-              <Header.Content>ScripeChat</Header.Content>
+              <Header.Content>DevChat</Header.Content>
             </Header>
           </Grid.Row>
-          {/* User Dropdown */}
+
+          {/* User Dropdown  */}
           <Header style={{ padding: '0.25em' }} as='h4' inverted>
             <Dropdown
-              trigger={<span>User</span>}
+              trigger={<span>{this.props.currentUser.displayName}</span>}
               options={this.dropdownOptions()}
             />
           </Header>
