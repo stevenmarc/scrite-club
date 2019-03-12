7a79ba67b26dd1cdb1fc66723cbf317ed7560905
stevenmarc <stevenmarcfr@gmail.com>
2019-03-12 16:37:38 +0100

    Display channels with child_added listener

--- src/components/SidePanel/Channels.js
+++ src/components/SidePanel/Channels.js
@@ -12,6 +12,18 @@ class Channels extends React.Component {
     modal: false
   };
 
+  componentDidMount() {
+    this.addListeners();
+  }
+
+  addListeners = () => {
+    let loadedChannels = [];
+    this.state.channelsRef.on('child_added', snap => {
+      loadedChannels.push(snap.val());
+      this.setState({ channels: loadedChannels });
+    });
+  };
+
   addChannel = () => {
     const { channelsRef, channelName, channelDetails, user } = this.state;
     const key = channelsRef.push().key;
@@ -49,6 +61,18 @@ class Channels extends React.Component {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  displayChannels = channels =>
+    channels.length > 0 &&
+    channels.map(channel => (
+      <Menu.Item
+        key={channel.id}
+        onClick={() => console.log(channel)}
+        name={channel.name}
+        style={{ opacity: 0.7 }}
+      >
+        # {channel.name}
+      </Menu.Item>
+    ));
   isFormValid = ({ channelName, channelDetails }) =>
     channelName && channelDetails;
 
@@ -69,7 +93,7 @@ class Channels extends React.Component {
             ({channels.length})
             <Icon name='add' onClick={this.openModal} />
           </Menu.Item>
-          {/*Channels*/}
+          {this.displayChannels(channels)}
         </Menu.Menu>
         l
         <Modal basic open={modal} onClose={this.closeModal}>
