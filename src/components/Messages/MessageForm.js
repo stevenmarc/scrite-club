f4b1867936a034754a1cc997e3983fa51f3c1aa4
stevenmarc <stevenmarcfr@gmail.com>
2019-03-15 15:18:38 +0100

    Add ProgressBar component

--- src/components/Messages/MessageForm.js
+++ src/components/Messages/MessageForm.js
@@ -4,6 +4,7 @@ import { Segment, Button, Input } from 'semantic-ui-react';
 import uuidv4 from 'uuid/v4';
 
 import FileModal from './FileModal';
+import ProgressBar from './ProgressBar';
 
 class MessageForm extends React.Component {
   state = {
@@ -135,7 +136,14 @@ class MessageForm extends React.Component {
   };
 
   render() {
-    const { errors, message, loading, modal } = this.state;
+    const {
+      errors,
+      message,
+      loading,
+      modal,
+      uploadState,
+      percentUploaded
+    } = this.state;
 
     return (
       <Segment className='message__form'>
@@ -170,12 +178,16 @@ class MessageForm extends React.Component {
             labelPosition='right'
             icon='cloud upload'
           />
-          <FileModal
-            modal={modal}
-            closeModal={this.closeModal}
-            uploadFile={this.uploadFile}
-          />
         </Button.Group>
+        <FileModal
+          modal={modal}
+          closeModal={this.closeModal}
+          uploadFile={this.uploadFile}
+        />
+        <ProgressBar
+          uploadState={uploadState}
+          percentUploaded={percentUploaded}
+        />
       </Segment>
     );
   }
