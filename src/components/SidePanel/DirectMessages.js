1fe7ba1c924e1c6f35254e5cd773efcfb06d772b
stevenmarc <stevenmarcfr@gmail.com>
2019-03-20 10:42:57 +0100

    Put DirectMessage Channel Data in Global State, Set as Private Channels

--- src/components/SidePanel/DirectMessages.js
+++ src/components/SidePanel/DirectMessages.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import firebase from '../../firebase';
+import { setCurrentChannel, setPrivateChannel } from '../../actions';
 import { Menu, Icon } from 'semantic-ui-react';
-import { userInfo } from 'os';
+
 class DirectMessages extends React.Component {
   state = {
     users: [],
@@ -65,6 +67,23 @@ class DirectMessages extends React.Component {
 
   isUserOnline = user => user.status === 'online';
 
+  changeChannel = user => {
+    const channelId = this.getChannelId(user.uid);
+    const channelData = {
+      id: channelId,
+      name: user.name
+    };
+    this.props.setCurrentChannel(channelData);
+    this.props.setPrivateChannel(true);
+  };
+
+  getChannelId = userId => {
+    const currentUserId = this.state.user.uid;
+    return userId < currentUserId
+      ? `${userId}/${currentUserId}`
+      : `${currentUserId}/${userId}`;
+  };
+
   render() {
     const { users } = this.state;
 
@@ -79,7 +98,7 @@ class DirectMessages extends React.Component {
         {users.map(user => (
           <Menu.Item
             key={user.uid}
-            onClick={() => console.log(user)}
+            onClick={() => this.changeChannel(user)}
             style={{ opacity: 0.7, fontStyle: 'italic' }}
           >
             <Icon
@@ -94,4 +113,7 @@ class DirectMessages extends React.Component {
   }
 }
 
-export default DirectMessages;
+export default connect(
+  null,
+  { setCurrentChannel, setPrivateChannel }
+)(DirectMessages);
